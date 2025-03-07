a8cb3a28714c92b4d1fb444be082b018b4bc68df
hunter <hunter411w@gmail.com>
2025-03-06 19:19:13 -0700

    Finalized news API & encorporated it into the landing page

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,14 +1,29 @@
 # User routing will go here, home, account, etc...
-
 from flask import Flask, render_template
 from flask_app import app
+import requests
+import os
+
+API_KEY = os.environ.get('NEWS_API')
+
+BASE_URL = "https://newsapi.org/v2/top-headlines"
+
+# Parameters for the request
+params = {
+    "sources": "espn, bleacher-report, sports-illustrated",         # ESPN source ID
+    "apiKey": API_KEY,        # API key
+    "pageSize": 2,
+}
+
+# Make the GET request
+response = requests.get(BASE_URL, params=params)
+data = response.json()
+print(data)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-
+    return render_template('index.html', article1=data['articles'][0], article2=data['articles'][1])
 
 
 @app.route('/sign-up')
