2f6b07aa583329a8538a59753a0f7c1d048369ab
Jeff Marc <jeff_marc@shi.com>
2025-03-06 19:10:06 -0500

    add more to models and user controller

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,9 +1,36 @@
 # User routing will go here, home, account, etc...
 
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect,flash,session,request
 from flask_app import app
+from flask_app.models.user import users
+from flask_bcrypt import Bcrypt
+import re
+
+bcrypt = Bcrypt(app)
+
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/register', methods = ['POST'])
+def register():
+    data = {
+        'first_name':request.form['first_name'],
+        'last_name':request.form['last_name'],
+        'email':request.form['email'],
+        'passwd':None
+    }
+    if isValid(data,request.form['passwd']) != True:
+        return redirect('/register')
+        
+    return redirect('/dashboard')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    data = {
+        'email': request.form['email']
+    }
+    user_info =  users.get_user_by_email(data)
+    return redirect('/dashboard')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,1 +1,75 @@
 # User class should go here.
+from flask_app.config.mysqlconnection import connect_to_mysql
+from flask import flash
+import re
+
+
+email_regex = re.compile('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z],{2,}$')
+passwd_regex = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$")   
+class users:
+  
+    db = "sport_scope_schema"
+    def __init__(self,data):
+        self.first_name =  data['first_name']
+        self.last_name =  data['last_name']
+        self.email = data['email']
+        self.passwd =data['passwd']
+        self.created_at =data['created_at']
+        self.updated_at = data['updatede_at']
+    
+    @classmethod
+    def get_users(cls):
+        query = """SELECT * FROM users;"""
+        returnedInfo = connect_to_mysql(cls.db).query_db(query)
+        return returnedInfo
+    
+        
+    @classmethod
+    def get_user_by_email(cls, data):
+        query = """SELECT * FROM users WHERE email= %(email)s;"""
+        returnedInfo = connect_to_mysql(cls.db).query_db(query,data)
+        return returnedInfo
+    
+        
+    @classmethod
+    def add_user(cls,data):
+        query = """INSERT INTO users(first_name,last_name,email,passwd,created_at) values(%(first_name)s,%(last_name)s,%(email)s,%(passwd)s,NOW());"""
+        returnedInfo = connect_to_mysql(cls.db).query_db(query,data)
+        return returnedInfo
+    
+        
+    @classmethod
+    def update_user(cls,data):
+        query = """UPDATE users SET 'first_name =%(first_name)s,last_name=%(last_name)s,email=%(email)s,updated_at = NOW() WHERE id= %(id)s;"""
+        returnedInfo = connect_to_mysql(cls.db).query_db(query,data)
+        return returnedInfo
+    
+    @classmethod
+    def update_user_password(cls,data):
+        query = """UPDATE users SET 'passwd =%(passwd)s,updated_at = NOW() WHERE email= %(email)s;"""
+        returnedInfo = connect_to_mysql(cls.db).query_db(query,data)
+        return returnedInfo
+    
+    @classmethod
+    def update_user_email(cls,data):
+        query = """UPDATE users SET email=%(email)s,updated_at = NOW() WHERE id= %(id)s;"""
+        returnedInfo = connect_to_mysql(cls.db).query_db(query,data)
+        return returnedInfo
+    
+    @staticmethod
+    def isValid(data,passwd=None,category=None):
+        valid = True
+        if len(data['first_name'])<2:
+            flash('You First Name must be longer than 2',category)
+            valid = False
+        if len(data['last_name'])<2:
+            flash('You Last Name must be longer than 2',category)
+            valid = False
+        if email_regex(data['email']) == False:
+            flash('Enter a valid Email',category)
+            valid = False
+            
+        if passwd_regex(passwd) == False:
+            flash('Enter a valid password',category)
+            valid = False
+        return valid
