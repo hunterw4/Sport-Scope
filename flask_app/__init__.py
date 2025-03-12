691dec54faf71b2fa76f165f885d8778a69d21d5
Samuel Coulon <samuelcoulonsk8@gmail.com>
2025-03-12 12:39:17 -0600

    Changed init to check if enviorment is working

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 import os
 
-app = Flask(__name__)
-app.secret_key = os.environ.get('DATA_SECRET_KEY')
-bcrypt = Bcrypt(app)
+secret_key = os.environ.get('DATA_SECRET_KEY')
+
+if secret_key:
+    app = Flask(__name__)
+    app.secret_key = secret_key
+    bcrypt = Bcrypt(app)
+else:
+    print("Enviorment Variable Not Found")
